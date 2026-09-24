04f716ec556a28731cb3df89f1d422a2c7cb2524
Caroline Hoffman <160681391+hoffman13caroline@users.noreply.github.com>
2026-09-24 13:37:08 -0400

    making bank acount, index and java start

---
+++ lab4/script.js
@@ -0,0 +1,39 @@
+let playerHealth = 1000;
+const withdrawAmount = 100;
+const depositAmount = 100;
+
+function withdraw() {
+    playerHealth = playerHealth - withdrawAmount;
+
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+healthText.innerText = playerHealth;
+
+
+if(playerHealth > 0)
+{
+    healthText.innerText = playerHealth;
+    statusText.innerText = "You've Been Hit";
+}
+else
+{
+healthText.innerText = 0;
+statusText.innerText = "Game Over!";
+statusText.style.color = "#f9331d";
+statusText.style.fontWeight = "bold";
+
+document.body.style.backgroundColor = "#5a1a1a";
+
+document.querySelector("button").disabled = true;
+document.querySelector("button").innerText = "Dead";
+}
+}
+
+function deposit() {
+    playerHealth = playerHealth + depositAmount;
+
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+healthText.innerText = playerHealth;
