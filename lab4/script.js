eba30c05de72d99bf273dfc6b8f054766b5b0a62
Caroline Hoffman <160681391+hoffman13caroline@users.noreply.github.com>
2026-09-24 14:19:02 -0400

    try to fix buttons

--- lab4/script.js
+++ lab4/script.js
@@ -25,8 +25,8 @@ statusText.style.fontWeight = "bold";
 
 document.body.style.backgroundColor = "#2f4f4f";
 
-document.querySelector("subtract").disabled = true;
-document.querySelector("subtract").innerText = "No Money to Withdraw";
+document.querySelector(button id = "subtract").disabled = true;
+document.querySelector(button id = "subtract").innerText = "No Money to Withdraw";
 }
 }
 
