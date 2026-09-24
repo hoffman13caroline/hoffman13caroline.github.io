84c50654b5784cec160b33aeb3f293d9e910f3a1
Caroline Hoffman <160681391+hoffman13caroline@users.noreply.github.com>
2026-09-24 13:48:47 -0400

    script fix by moving order of code

--- lab4/script.js
+++ lab4/script.js
@@ -9,24 +9,10 @@ function withdraw() {
     const statusText = document.getElementById("status-message");
 
 healthText.innerText = playerHealth;
-
-
 if(playerHealth < 1000)
 {
     healthText.innerText = playerHealth;
     statusText.innerText = "Money Withdrawn";
-}
-else
-{
-healthText.innerText = 0;
-statusText.innerText = "No Balance - $0";
-statusText.style.color = "#f9331d";
-statusText.style.fontWeight = "bold";
-
-document.body.style.backgroundColor = "#5a1a1a";
-
-document.querySelector("button").disabled = true;
-document.querySelector("button").innerText = "Dead";
 }
 }
 
@@ -41,6 +27,17 @@ if(playerHealth > 1000)
     healthText.innerText = playerHealth;
     statusText.innerText = "Money Deposited";
 }
-
 healthText.innerText = playerHealth;
+}
+else
+{
+healthText.innerText = 0;
+statusText.innerText = "No Balance $0";
+statusText.style.color = "#f9331d";
+statusText.style.fontWeight = "bold";
+
+document.body.style.backgroundColor = "#5a1a1a";
+
+document.querySelector("button").disabled = true;
+document.querySelector("button").innerText = "No Balance $0";
 }
