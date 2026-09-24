c6f4db955880eb1ef0c0d07bf7148de56b4307a4
Caroline Hoffman <160681391+hoffman13caroline@users.noreply.github.com>
2026-09-24 14:03:12 -0400

    back to original changes

--- lab4/script.js
+++ lab4/script.js
@@ -10,23 +10,24 @@ function withdraw() {
 
 healthText.innerText = playerHealth;
 
-if(playerHealth < 0)
+
+if(playerHealth < 1000)
 {
     healthText.innerText = playerHealth;
     statusText.innerText = "Money Withdrawn";
 }
-}
 else
 {
 healthText.innerText = 0;
-statusText.innerText = 0;
+statusText.innerText = "No Balance - $0";
 statusText.style.color = "#f9331d";
 statusText.style.fontWeight = "bold";
 
 document.body.style.backgroundColor = "#5a1a1a";
 
 document.querySelector("button").disabled = true;
-document.querySelector("button").innerText = "No Balance $0";
+document.querySelector("button").innerText = "Dead";
+}
 }
 
 function deposit() {
