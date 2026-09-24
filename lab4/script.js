275147b6897218e7eb43d9e2e7ab042f87a14a63
Caroline Hoffman <160681391+hoffman13caroline@users.noreply.github.com>
2026-09-24 13:56:40 -0400

    revomed else for time being

--- lab4/script.js
+++ lab4/script.js
@@ -10,23 +10,10 @@ function withdraw() {
 
 healthText.innerText = playerHealth;
 
-
 if(playerHealth < 1000)
 {
     healthText.innerText = playerHealth;
     statusText.innerText = "Money Withdrawn";
-}
-else
-{
-healthText.innerText = 0;
-statusText.innerText = "No Balance $0";
-statusText.style.color = "#f9331d";
-statusText.style.fontWeight = "bold";
-
-document.body.style.backgroundColor = "#5a1a1a";
-
-document.querySelector("button").disabled = true;
-document.querySelector("button").innerText = "No Balance $0";
 }
 }
 
